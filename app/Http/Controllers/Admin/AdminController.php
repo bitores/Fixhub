de39a58df15384cf2f96d0a9942517897179b0bf
lianguan <guan.shiliang@gmail.com>
2017-09-08 12:03:17 +0800

    Added database show

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -28,12 +28,12 @@ class AdminController extends Controller
         $title = trans('admin.dashboard');
         $envs = [
             ['name' => 'Fixhub version',    'value' => APP_VERSION],
-            ['name' => 'PHP version',       'value' => 'PHP/'.PHP_VERSION],
+            ['name' => 'PHP version',       'value' => PHP_VERSION],
             ['name' => 'Laravel version',   'value' => app()->version()],
             ['name' => 'CGI',               'value' => php_sapi_name()],
-            ['name' => 'Uname',             'value' => php_uname()],
             ['name' => 'Server',            'value' => $_SERVER['SERVER_SOFTWARE']],
 
+            ['name' => 'Database',          'value' => config('database.default')],
             ['name' => 'Cache driver',      'value' => config('cache.default')],
             ['name' => 'Session driver',    'value' => config('session.driver')],
             ['name' => 'Queue driver',      'value' => config('queue.default')],
